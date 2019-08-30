5b2db21be53bc4dcc11cb786b9aec7478c7b979a
Xmader <xmader@outlook.com>
2019-08-30 14:59:24 -0400

    debug

--- tools/post_build_apk.sh
+++ tools/post_build_apk.sh
@@ -1,3 +1,3 @@
 APK_NAME=$(node -p "const {version,displayName}=require('./package.json');displayName+'-v'+version")
 mkdir -p dist
-mv ./platforms/android/build/outputs/apk/android-*.apk ./dist/$APK_NAME.apk
+mv ./platforms/android/app/build/outputs/apk/debug/*.apk ./dist/$APK_NAME.apk
